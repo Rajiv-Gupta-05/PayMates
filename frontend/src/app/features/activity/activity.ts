6b6b6574b1ee9ce740e5a058cbfd6735341035c6
Rajiv Gupta <rajiv.gupta@acldigital.com>
2026-05-09 00:38:27 +0530

    Added Frontend Project.

---
+++ frontend/src/app/features/activity/activity.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity',
+  imports: [],
+  templateUrl: './activity.html',
+  styleUrl: './activity.scss',
+})
+export class Activity {}
